96275f0b0783e62ed6ae3b2647bce7e4be5c2fbf
Chenyibin <Chenyibin@qq.com>
2020-12-07 20:36:45 +0800

    yes

--- src/main/java/com/chen/forum/controller/IndexController.java
+++ src/main/java/com/chen/forum/controller/IndexController.java
@@ -1,16 +1,14 @@
 package com.chen.forum.controller;
 
-import com.fasterxml.jackson.core.sym.Name1;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name", name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
